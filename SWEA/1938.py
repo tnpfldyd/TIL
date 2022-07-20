227227cc92efb7c866bd0c23191b4acc832a0553
tnpfldyd <dkdldhs0322@gmail.com>
2022-07-20 15:52:40 +0900

    7/20 SW 추가

---
+++ SWEA/1938.py
@@ -0,0 +1,4 @@
+a, b = map(int, input().split())
+
+for e in [a+b, a-b, a*b, a//b]:
+    print(e)
---
+++ SWEA/2043.py
@@ -0,0 +1,5 @@
+a, b = map(int, input().split())
+result = 0
+for i in range(b, a+1):
+    result += 1
+print(result)
