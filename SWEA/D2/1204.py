d4659b99586cbf0c267a10a0aa8cabdf560ba873
tnpfldyd <dkdldhs0322@gmail.com>
2022-07-26 17:04:35 +0900

    7/26 SWEA 문제풀이

--- SWEA/D2/1204.py
+++ SWEA/D2/1204.py
@@ -10,5 +10,5 @@ for i in range(T):
         if j not in F:
             F[j] = 0
         F[j] += 1
-    max_F = max(F , key = F.get)
+    max_F = max(F , key = F.get) # 벨류가 제일 높은 키 찾는 코드
     print('#'+A, max_F)
