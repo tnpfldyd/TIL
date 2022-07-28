c46154fe1f55fb985ecf3abf609484ef49d65d5d
tnpfldyd <dkdldhs0322@gmail.com>
2022-07-28 18:06:47 +0900

    설명추가

--- baekjoon/1302.py
+++ baekjoon/1302.py
@@ -6,5 +6,5 @@ for i in range(1, T+1):
         result[a] += 1
     else:
         result[a] = 1
-best = sorted(result.items(), key = lambda x : (-x[1], x[0]))
+best = sorted(result.items(), key = lambda x : (-x[1], x[0])) # -x[1] 은 벨류값을 내림차순 # x[0] 은 키값을 오름차순
 print(best[0][0])
