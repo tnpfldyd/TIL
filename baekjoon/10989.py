ec1b4d2f6aeaa66b2e41b14b8541ea503c6e1a3a
tnpfldyd <dkdldhs0322@gmail.com>
2022-08-02 10:22:40 +0900

    0802

--- baekjoon/10989.py
+++ baekjoon/10989.py
@@ -1,9 +1,10 @@
 import sys
 input = sys.stdin.readline
 T = int(input())
-result = []
+result = [0] * 10001
 for i in range(T):
-    number = int(input())
-    result.append(number)
-result.sort()
-print(*result, sep='\n')
+    result[int(input())] += 1
+for j in range(10001):
+    if result[j] != 0:
+        for k in range(result[j]):
+            print(j)
