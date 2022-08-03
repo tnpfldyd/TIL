e9df6125ea4a4ffaa8226bb522c04c8cffb6f06a
tnpfldyd <dkdldhs0322@gmail.com>
2022-08-03 16:59:38 +0900

    SWEA 추가

--- SWEA/D2/2001.py
+++ SWEA/D2/2001.py
@@ -3,9 +3,15 @@ import sys
 sys.stdin = open("2001input.txt", "r")
 
 T = int(input())
-result = 0
 for i in range(1, T+1):
-    a, b = map(int, input().split())
-    for j in range(a):
-        c = list(map(int, input().split()))
-        
+    N, M = map(int, input().split())
+    matrix = [list(map(int, input().split())) for _ in range(N)]
+    result = []
+    for j in range(N-M+1):
+        for k in range(N-M+1):
+            bug = 0
+            for q in range(M):
+                for p in range(M):
+                    bug += matrix[j+q][k+p]
+            result.append(bug)
+    print(f'#{i}', max(result))
