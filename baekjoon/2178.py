67c25411f14f7ba371c7805d11c442ef8c0e6bae
tnpfldyd <dkdldhs0322@gmail.com>
2022-08-08 23:19:57 +0900

    0808

--- baekjoon/2178.py
+++ baekjoon/2178.py
@@ -1,24 +1,19 @@
 N, M = map(int, input().split())
 matrix = [list(map(int, input())) for _ in range(N)]
-low = [1,0,-1]
-col = [0,1,0]
-for x in range(N):
-    for y in range(M):
-        if matrix[x][y] != 0:
-            for k in range(3):
-                nx = x + low[k]
-                ny = y = col[k]
-                cnt = 1
-                while 0 <= nx < M and 0 <= ny < N and matrix[nx][ny] == matrix[x][y]:
-                    cnt += 1
-                    nx += low[k]
-                    ny += col[k]
-                if 0 <= nx < M and 0 <= ny < N and matrix[nx][ny] != matrix[x][y]:
-                    for j in range(3):
-                        jx = nx + low[k]
-                        jy = ny + col[y]
-                        while 0 <= jx < M and 0 <= jy < N and matrix[jx][jy] == matrix[x][y]:
-                            cnt += 1
-                            jx += low[k]
-                            jy += col[y]
-                            
+result = [[0]*M for _ in range(N)]
+dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
+start = [(N-1, M-1)]
+result[N-1][M-1] = 1
+
+while start:
+    x, y = start.pop(0)
+    if x == 0 and y == 0:
+        print(result[x][y])
+        break
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < M:
+            if result[nx][ny] == 0 and matrix[nx][ny] == '1':
+                result[nx][ny] = result[x][y] + 1
+                start.append((nx, ny))
