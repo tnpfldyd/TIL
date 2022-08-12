55a1858165a2b621f006f1d1d79d88bc80e8d8e7
tnpfldyd <dkdldhs0322@gmail.com>
2022-08-13 03:56:18 +0900

    0813

---
+++ baekjoon/2635.py
@@ -0,0 +1,20 @@
+T = int(input())
+result = [[T] for _ in range(T)]
+for i in range(T):
+    result[i].append(result[i][0] - i)
+    start = 0
+    minus = 1
+    stack = [result[i][minus]]
+    while stack:
+        suyeol = stack.pop()
+        if suyeol < 0:
+            break
+        kcats = result[i][start] - result[i][minus]
+        if kcats >= 0:
+            stack.append(kcats); result[i].append(kcats)
+            start += 1; minus += 1
+temp = max(map(len, (result)))
+for i in range(T):
+    if len(result[i]) == temp:
+        print(temp)
+        print(*result[i])
--- baekjoon/test.py
+++ baekjoon/test.py
@@ -1,39 +1,19 @@
-from pprint import pprint
 import sys
-sys.stdin = open('test.txt', 'r')
+from collections import Counter
 
-N, M = map(int, input().split())
-matrix = [input().split() for _ in range(N)]
-visited = [[0] * M for _ in range(N)]
-dx, dy = [0, 1, 0, -1],[1, 0, -1, 0] # 우 하 좌 상
-for x in range(N):
-    for y in range(M):
-        if matrix[x][y] == '0' and visited[x][y] == 0:
-            start = [(0, 0)]
-            visited[x][y] = 1
-            while start:
-                a, b = start.pop()
-                for i in range(4):
-                    nx = a + dx[i]
-                    ny = b + dy[i]
-                    if 0 <= nx < N and 0 <= ny < M:
-                        if matrix[nx][ny] == '0' and visited[nx][ny] == 0:
-                            visited[nx][ny] = visited[a][b] + 1
-                            start.append((nx, ny))                          
-                            if nx == N-1 and ny == M -1:
-                                break
-start2 = [(N-1, M -1)]
-result = []
-result.append((N-1, M -1))
-while start2:
-    x, y = start2.pop()
-    if x == 0 and y == 0:
-        break
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < N and 0 <= ny < M:
-            if visited[x][y] -1 == visited[nx][ny]:
-                result.append((nx, ny))
-                start2.append((nx, ny))
-print(result[::-1])
+# sys.stdin = open("input_text/_반반.txt")
+
+T = int(input())
+
+answer = 'Yes'
+for test_case in range(1, T + 1):
+    S = Counter(input())
+
+    if len(S) != 2:
+        answer = 'No'
+
+    for cnt in S.values():
+        if cnt != 2:
+            answer = 'No'
+
+    print(f'#{test_case} {answer}')
