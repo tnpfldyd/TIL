d40e6c5b1b9b925a1425d8ac1ba189941a3199bd
tnpfldyd <dkdldhs0322@gmail.com>
2022-08-16 20:50:52 +0900

    0816SWEA;

---
+++ SWEA/D4/1210.py
@@ -0,0 +1,37 @@
+from pprint import pprint
+import sys
+sys.stdin = open('1210input.txt', 'r')
+T = 10
+for i in range(1, T+1):
+    N = int(input())
+    matrix = [list(map(int, input().split())) for _ in range(100)]
+    dx, dy = [0, 0, -1], [-1, 1, 0]
+    s = 2
+    start = []
+    for k in range(100):
+        if matrix[99][k] == 2:
+            start.append((99, k))
+    # print(start)
+    while start:
+        x, y = start.pop()
+        if x == 0:
+            print(f'#{i} {y}')
+            break
+        if s == 2:
+            for j in range(2):
+                nx = x + dx[j]; ny = y + dy[j]
+                if 0 <= ny < 100 and matrix[nx][ny] == 1:
+                    s = j
+                    start.append((nx, ny))
+                    break
+            else:
+                nx = x + dx[s]; ny = y + dy[s]
+                start.append((nx, ny))
+        elif s == 1 or s == 0:
+            if 0 <= y + dy[s] < 100 and matrix[x + dx[s]][y + dy[s]] == 1:
+                nx = x + dx[s]; ny = y + dy[s]
+                start.append((nx, ny))
+            else:
+                s = 2
+                nx = x + dx[s]; ny = y + dy[s]
+                start.append((nx, ny))
