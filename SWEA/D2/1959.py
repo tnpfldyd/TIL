7de6bd0fc35ccf3e38e8844a2467239291807607
tnpfldyd <dkdldhs0322@gmail.com>
2022-08-16 17:13:41 +0900

    0816

--- SWEA/D2/1959.py
+++ SWEA/D2/1959.py
@@ -1,2 +1,16 @@
 import sys
 sys.stdin = open("1959input.txt", "r")
+T = int(input())
+for i in range(1, T+1):
+    N, M = map(int, input().split())
+    result = [[] for _ in range(abs(N-M) + 1)]
+    if N > M:
+        a = list(map(int, input().split()))
+        b = list(map(int, input().split()))
+    else:
+        b = list(map(int, input().split()))
+        a = list(map(int, input().split()))
+    for j in range(abs(N - M) + 1):
+        for k in range(min(N, M)):
+            result[j].append(b[k]* a[k + j])
+    print(f'#{i} {max(map(sum, result))}')
--- SWEA/D2/test.py
+++ SWEA/D2/test.py
@@ -1,2 +1,28 @@
-a = list(input())
-print(a, type(a))
+import sys
+input = sys.stdin.readline
+N = int(input())
+M = int(input())
+N += 1
+
+matrix = [[] for _ in range(N)] # 리스트 생성
+for _ in range(M):
+    v1, v2 = map(int, input().rstrip().split())
+    matrix[v1].append(v2)
+    matrix[v2].append(v1)
+visited = [False] * N
+stack = [1]
+visited[1] = True
+result = set()
+cnt = 0
+while stack:
+    virus = stack.pop()
+    for i in matrix[virus]:
+        if not visited[i]:
+            visited[i] = True
+            stack.append(i)
+            result.add(i)
+            cnt += 1
+
+print(sum(visited) - 1)
+print(len(result))
+print(cnt)
