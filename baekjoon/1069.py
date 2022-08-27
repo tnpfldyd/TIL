28f4698ca6711a1cc2cfa7e209153afcd1049a4d
tnpfldyd <dkdldhs0322@gmail.com>
2022-08-27 11:49:18 +0900

    0827

---
+++ baekjoon/1069.py
@@ -0,0 +1,11 @@
+chess = []
+cnt = 0
+
+for i in range(8):
+    chess.append(list(str(input())))
+
+for j in range(8):
+    for k in range(8):
+        if (j+k) % 2 == 0 and chess[j][k] == "F":
+                cnt += 1
+print(cnt)
--- baekjoon/10769.py
+++ baekjoon/10769.py
@@ -1,12 +1,30 @@
 Text = input()
-happy = Text.count(':-)')
-sad = Text.count(':-(')
-if happy == 0 and sad == 0:
+# happy = Text.count(':-)')
+# sad = Text.count(':-(')
+# if happy == 0 and sad == 0:
+#     print('none')
+# else:
+#     if happy > sad:
+#         print('happy')
+#     elif sad > happy:
+#         print('sad')
+#     else:
+#         print('unsure')
+result = []
+a, b = 0, 0
+for i in Text:
+    if i == ')':
+        if result[-1] == '-' and result[-2] == ':':
+            a += 1
+    elif i == '(':
+        if result[-1] == '-' and result[-2] == ':':
+            b += 1
+    result.append(i)
+if a == 0 and b == 0:
     print('none')
-else:
-    if happy > sad:
-        print('happy')
-    elif sad > happy:
-        print('sad')
-    else:
-        print('unsure')
+elif a > b:
+    print('happy')
+elif a < b:
+    print('sad')
+elif a == b:
+    print('unsure')
