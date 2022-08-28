29e6f22ec3fb844e3a3bec043c889ac703873525
tnpfldyd <dkdldhs0322@gmail.com>
2022-08-28 10:18:17 +0900

    0828

--- baekjoon/10769.py
+++ baekjoon/10769.py
@@ -1,30 +1,12 @@
 Text = input()
-# happy = Text.count(':-)')
-# sad = Text.count(':-(')
-# if happy == 0 and sad == 0:
-#     print('none')
-# else:
-#     if happy > sad:
-#         print('happy')
-#     elif sad > happy:
-#         print('sad')
-#     else:
-#         print('unsure')
-result = []
-a, b = 0, 0
-for i in Text:
-    if i == ')':
-        if result[-1] == '-' and result[-2] == ':':
-            a += 1
-    elif i == '(':
-        if result[-1] == '-' and result[-2] == ':':
-            b += 1
-    result.append(i)
-if a == 0 and b == 0:
+happy = Text.count(':-)')
+sad = Text.count(':-(')
+if happy == 0 and sad == 0:
     print('none')
-elif a > b:
-    print('happy')
-elif a < b:
-    print('sad')
-elif a == b:
-    print('unsure')
+else:
+    if happy > sad:
+        print('happy')
+    elif sad > happy:
+        print('sad')
+    else:
+        print('unsure')
