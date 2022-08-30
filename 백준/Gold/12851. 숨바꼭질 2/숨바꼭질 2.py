4e4b0c620e12eeb82f1bedf008281630ff73bcf6
Yong <108651414+tnpfldyd@users.noreply.github.com>
2022-08-30 20:36:43 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 184 ms, Memory: 126088 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -2,7 +2,7 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 N, K = map(int, input().rstrip().split())
-list_ = [[-1, 0] for _ in range(100001)]
+list_ = [[0, 0] for _ in range(100001)]
 start = deque()
 start.append(N)
 list_[N][0] = 0
@@ -15,7 +15,7 @@ while start:
         break
     for i in (x-1, x+1, x*2):
         if 0 <= i < 100001:
-            if list_[i][0] == -1:
+            if list_[i][0] == 0:
                 list_[i][0] = list_[x][0] + 1
                 list_[i][1] = list_[x][1]
                 start.append(i)
