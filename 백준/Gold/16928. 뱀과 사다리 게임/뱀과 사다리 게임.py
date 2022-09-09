e5ca98489f5f3e536167e9f1d0da6078e9852856
Yong <108651414+tnpfldyd@users.noreply.github.com>
2022-09-09 18:20:50 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 140 ms, Memory: 114280 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,36 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+N, M = map(int, input().split())
+N_dic, M_dic = {}, {}
+visited = [-1] * 101
+for i in range(N):
+    a, b = map(int, input().split())
+    N_dic[a] = b
+for i in  range(M):
+    a, b = map(int, input().split())
+    M_dic[a] = b
+visited[1] = 0
+start = deque()
+start.append(1)
+while start:
+    x = start.popleft()
+    if x == 100:
+        print(visited[x])
+        break
+    for i in range(1, 7):
+        nx = x + i
+        if 0 <= nx < 101 and visited[nx] == -1:
+            if N_dic.get(nx) != None:
+                nx = N_dic[nx]
+                if visited[nx] == -1:
+                    visited[nx] = visited[x] + 1
+                    start.append(nx)
+            elif M_dic.get(nx) != None:
+                nx = M_dic[nx]
+                if visited[nx] == -1:
+                    visited[nx] = visited[x] + 1
+                    start.append(nx)
+            else:
+                visited[nx] = visited[x] + 1
+                start.append(nx)
