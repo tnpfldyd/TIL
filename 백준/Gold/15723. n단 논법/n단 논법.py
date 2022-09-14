6cd2fec3fa35c28cf8a4a7eca453eed782a4ff0a
Yong <108651414+tnpfldyd@users.noreply.github.com>
2022-09-14 16:45:35 +0900

    [Gold V] Title: n단 논법, Time: 120 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Gold/15723. n단 논법/n단 논법.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+matrix = [[0] * 26 for _ in range(26)]
+for i in range(N):
+    a, b = input().rstrip().split(' is ')
+    matrix[ord(a)-97][ord(b)-97] = 1
+for k in range(26):
+    for i in range(26):
+        for j in range(26):
+            if matrix[i][j] == 1 or (matrix[i][k] == 1 and matrix[k][j] == 1):
+                matrix[i][j] = 1
+T = int(input())
+for i in range(T):
+    a, b = input().rstrip().split(' is ')
+    if matrix[ord(a)-97][ord(b)-97] == 1:
+        print('T')
+    else:
+        print('F')
