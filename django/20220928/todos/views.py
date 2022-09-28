dcf7deaa920810a808e9c74ee33227ca7fdacf0e
tnpfldyd <dkdldhs0322@gmail.com>
2022-09-28 17:53:59 +0900

    0928

---
+++ django/20220928/todos/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render,redirect
+from .models import Todo
+# Create your views here.
+def index(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos,
+    }
+    return render(request, 'todos/index.html', context)
+
+def create(request):
+    content = request.GET
+    if content['priority'] == 'Open this select menu':
+        Todo.objects.create(content=content['content'], deadline=content['deadline'])
+    else:
+        Todo.objects.create(content=content['content'], priority=content['priority'], deadline=content['deadline'])
+    return redirect('todos:index')
+
+def delete(request, todo_pk):
+    todo = Todo.objects.get(pk = todo_pk)
+    todo.delete()
+    return redirect('todos:index')
+
+def update(request, todo_pk):
+    todo = Todo.objects.get(pk = todo_pk)
+    if todo.completed == True:
+        todo.completed = False
+    else:
+        todo.completed = True
+    todo.save()
+    return redirect('todos:index')
