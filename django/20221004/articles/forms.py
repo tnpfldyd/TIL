4273b0b2511044405f913f7c395964178082b10d
tnpfldyd <dkdldhs0322@gmail.com>
2022-10-04 17:05:35 +0900

    1004

--- django/20221004/articles/forms.py
+++ django/20221004/articles/forms.py
@@ -1,8 +1,13 @@
-from dataclasses import field
 from django import forms
 from .models import Article
+from django.core.exceptions import ValidationError
 
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ['title', 'content']
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if '*' in title:
+            raise ValidationError('*은 제목에 들어갈 수 없습니다.')
+        return title
