1240d75c7f6b244b44d94c4767470d3e902a4439
Yong <108651414+tnpfldyd@users.noreply.github.com>
2022-10-08 14:35:39 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    s = s.lower()
+    answer = ''
+    cnt = 0
+    for i in s:
+        if cnt == 0 and i not in '0123456789':
+            answer += i.upper()
+        else:
+            answer += i
+        cnt += 1
+        if i == ' ':
+            cnt = 0
+    return answer
