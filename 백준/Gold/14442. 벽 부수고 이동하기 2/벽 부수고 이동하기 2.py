7e87464d8e6a7c30884ac5d41964e6713ebf4d68
Yong <108651414+tnpfldyd@users.noreply.github.com>
2022-12-03 18:07:50 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 4936 ms, Memory: 460960 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -0,0 +1,26 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+N, M, K= map(int,input().split())
+visited = [[[0] * (K + 1) for _ in range(M)] for _ in range(N)]
+matrix = [list(map(int,input().strip())) for _ in range(N)]
+dx, dy = [0,0,1,-1],[1,-1,0,0]
+visited[0][0][K] = 1
+start = deque()
+start.append((0,0,K))
+while start:
+    x, y, z = start.popleft()
+    if x == N-1 and y == M-1:
+        print(visited[x][y][z])
+        break
+    for i in range(4):
+        nx = x + dx[i]; ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < M:
+            if matrix[nx][ny] == 1 and z > 0 and visited[nx][ny][z-1] == 0: # z 값을 바꿔버리면 안됨 기억기억
+                visited[nx][ny][z-1] = visited[x][y][z] + 1
+                start.append((nx, ny, z-1))
+            elif matrix[nx][ny] == 0 and visited[nx][ny][z] == 0:
+                visited[nx][ny][z] = visited[x][y][z] + 1
+                start.append((nx, ny, z))
+else:
+    print(-1)
