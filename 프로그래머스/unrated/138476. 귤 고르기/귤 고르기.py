75480d7c30e68df4548aafd4dc793c5d0cb59c18
Yong <108651414+tnpfldyd@users.noreply.github.com>
2022-12-24 14:39:25 +0900

    [unrated] Title: 귤 고르기, Time: 30.06 ms, Memory: 21.6 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,15 +1,13 @@
 def solution(k, tangerine):
-    apple_dic = {}
+    orange_dic = {}
     for i in tangerine:
-        if i not in apple_dic:
-            apple_dic[i] = 1
+        if i not in orange_dic:
+            orange_dic[i] = 1
         else:
-            apple_dic[i] += 1
-    apple_dic = sorted(apple_dic.items(), key=lambda x : -x[1])
+            orange_dic[i] += 1
+    orange_dic = sorted(orange_dic.items(), key = lambda x : x[1])
     cnt = 0
-    for i, cost in apple_dic:
-        if k < 1:
-            break
-        k -= cost
+    while k > 0:
+        k -= orange_dic.pop()[1]
         cnt += 1
     return cnt
