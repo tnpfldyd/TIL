e086b4ea3f8124720c6bb39787bbc0cffb1f7b69
Yong <108651414+tnpfldyd@users.noreply.github.com>
2022-12-28 10:36:22 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -0,0 +1,11 @@
+def gcd(a, b):
+    if not b:
+        return a
+    if not a % b:
+        return b
+    return gcd(b, a % b)
+
+
+def solution(n):
+    answer = (n*6//gcd(n,6))//6
+    return answer
