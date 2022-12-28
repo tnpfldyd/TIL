2c0a9b7c325c8fb379de87fffa9280f91c09e2d9
Yong <108651414+tnpfldyd@users.noreply.github.com>
2022-12-28 10:54:24 +0900

    [Silver II] Title: 색종이 만들기, Time: 60 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+matrix = [list(map(int, input().strip().split())) for _ in range(N)]
+W_B = [0, 0]
+def solve(x, y, n):
+    cnt = 0
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if matrix[i][j]:
+                cnt += 1
+    if cnt == n*n:
+        W_B[1] += 1
+    elif not cnt:
+        W_B[0] += 1
+    else:
+        solve(x, y, n//2)#left top
+        solve(x, y + (n//2), n//2)#right top
+        solve(x + (n//2), y, n//2)#left bottom
+        solve(x + (n//2), y + (n//2), n//2)#right bottom
+    return
+solve(0, 0, N)
+for i in W_B:
+    print(i)
