9da2607635e6802bf6b0d68e3000b45e01dcf34d
Yong <108651414+tnpfldyd@users.noreply.github.com>
2022-12-29 15:45:56 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.17 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    min_num = 987654321
+    for i in arr:
+        min_num = min(min_num, i)
+    result = []
+    for i in arr:
+        if i != min_num:
+            result.append(i)
+    if not result:
+        result.append(-1)
+    return result
