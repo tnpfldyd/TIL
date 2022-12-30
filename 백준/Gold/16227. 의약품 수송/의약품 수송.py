95b11b45b30251e343b2fef05a279836d512e7e9
Yong <108651414+tnpfldyd@users.noreply.github.com>
2022-12-30 16:20:34 +0900

    [Gold I] Title: 의약품 수송, Time: 60 ms, Memory: 33052 KB -BaekjoonHub

--- 백준/Gold/16227. 의약품 수송/의약품 수송.py
+++ 백준/Gold/16227. 의약품 수송/의약품 수송.py
@@ -8,13 +8,13 @@ for _ in range(K):
     a, b, t = map(int, input().split())
     matrix[a].append((t, b))
     matrix[b].append((t, a))
-visited = [[INF] * 101 for _ in range(N+2)]
-visited[0][100] = 0
+visited = [INF] * (N+2)
+visited[0] = 0
 start = []
 heappush(start, (0, 0, 100))
 while start:
     cost, node, oil = heappop(start)
-    if cost > visited[node][oil]:
+    if cost > visited[node]:
         continue
     for k, v in matrix[node]:
         if k > 100:
@@ -25,7 +25,7 @@ while start:
         else:
             new_oil = 100 - k
             new_cost += 5
-        if visited[v][new_oil] > new_cost:
-            visited[v][new_oil] = new_cost
+        if visited[v] > new_cost:
+            visited[v] = new_cost
             heappush(start, (new_cost, v, new_oil))
-print(min(visited[N+1]))
+print(visited[N+1])
