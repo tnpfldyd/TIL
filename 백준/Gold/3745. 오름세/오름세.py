364ca7fdf6f53bc08d72b1b75f70621e883f6a1c
Yong <108651414+tnpfldyd@users.noreply.github.com>
2022-12-31 15:58:57 +0900

    [Gold II] Title: 오름세, Time: 160 ms, Memory: 129352 KB -BaekjoonHub

---
+++ 백준/Gold/3745. 오름세/오름세.py
@@ -0,0 +1,18 @@
+import sys, bisect
+input = sys.stdin.readline
+try:
+    while input():
+        N_list = list(map(int, input().strip().split()))
+        result = []
+        for n in N_list:
+            if result:
+                if result[-1] < n:
+                    result.append(n)
+                else:
+                    idx = bisect.bisect_left(result, n)
+                    result[idx] = n
+            else:
+                result.append(n)
+        print(len(result))
+except:
+    exit()
