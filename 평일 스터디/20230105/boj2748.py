969ca8263df46d48f1ae5e8fe63a8a16fa9c9c01
tnpfldyd <dkdldhs0322@gmail.com>
2023-01-05 17:14:33 +0900

    01/05 알고리즘

---
+++ 평일 스터디/20230105/boj2748.py
@@ -0,0 +1,10 @@
+N = int(input())
+n1 = 0
+n2 = 1
+result = 1
+if N > 2:
+    for i in range(2, N+1):
+        result = n1 + n2
+        n1 = n2
+        n2 = result
+print(result)
---
+++ 평일 스터디/20230105/prog120910.py
@@ -0,0 +1,5 @@
+def solution(n, t):
+    answer = n * (2 ** t)
+    return answer
+
+print(solution(7, 15))
