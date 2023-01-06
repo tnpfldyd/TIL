d1f1f56e868fba716d99981b32d5bf39db05ec78
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-01-06 17:20:19 +0900

    [Platinum V] Title: 0과 1, Time: 76 ms, Memory: 34688 KB -BaekjoonHub

---
+++ 백준/Platinum/8111. 0과 1/0과 1.py
@@ -0,0 +1,27 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    number = int(input())
+    visited = [''] * 20001
+    start = deque()
+    start.append(1)
+    visited[1] = '1'
+    answer = False
+    while start and not answer:
+        x = start.popleft()
+        if len(visited[x]) == 100:
+            print('BRAK')
+            break
+        for i in (0, 1):
+            next_number = (x * 10 + i) % number
+            if next_number:
+                if visited[next_number] == '':
+                    visited[next_number] = visited[x] + str(i)
+                    start.append(next_number)
+            else:
+                print(visited[x]+str(i))
+                answer = True
+                break
