d48fa2eb873059aa1b09593070289ff89eb7fdd2
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-01-10 11:40:03 +0900

    [Bronze I] Title: 디지털 티비, Time: 32 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2816. 디지털 티비/디지털 티비.py
+++ 백준/Bronze/2816. 디지털 티비/디지털 티비.py
@@ -1,7 +1,9 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
 chanels = []
 for _ in range(N):
-    chanels.append(input())
+    chanels.append(input().strip())
 i, j = chanels.index('KBS1'), chanels.index('KBS2')
 if i > j: 
     j += 1
