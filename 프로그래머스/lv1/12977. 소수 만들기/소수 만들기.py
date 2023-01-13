3576a36e030ad8398e967f7ef40168c9f7d8d7f6
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-01-13 15:24:03 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,16 @@
+import itertools
+
+def is_prime(x):
+    if x < 2:
+        return False
+    for i in range(2, int(x**0.5) + 1):
+        if not x % i:
+            return False
+    return True
+
+def solution(nums):
+    answer = 0
+    for i in itertools.combinations(nums, 3):
+        if is_prime(sum(i)):
+            answer += 1
+    return answer
