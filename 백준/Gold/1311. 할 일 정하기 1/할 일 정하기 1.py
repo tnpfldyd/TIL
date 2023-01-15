18bf38f2b93b2e55fd47520429672be883b82c98
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-01-15 18:27:28 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 1060 ms, Memory: 280048 KB -BaekjoonHub

---
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+INF = sys.maxsize
+N = int(input())
+matrix = [list(map(int, input().strip().split())) for _ in range(N)]
+dp = [[-1] * (1 << N) for _ in range(N)]
+
+def bit(node, visited):
+    if visited == (1 << N) - 1:
+        return 0
+    if dp[node][visited] != -1:
+        return dp[node][visited]
+    
+    dp[node][visited] = INF
+    for i in range(N):
+        if visited & (1 << i):
+            continue
+        dp[node][visited] = min(dp[node][visited], bit(node + 1, visited | (1 << i)) + matrix[node][i])
+    return dp[node][visited]
+
+print(bit(0, 0))
