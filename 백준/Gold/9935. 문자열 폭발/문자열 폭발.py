cdf63cd26174d06304416d00188078ac7b905fa3
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-01-16 15:29:18 +0900

    [Gold IV] Title: 문자열 폭발, Time: 444 ms, Memory: 41632 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,11 +1,10 @@
-import sys
-input = sys.stdin.readline
-q = list(input().rstrip())
-w = list(input().rstrip())
-final = []
-for i in q:
-    final.append(i)
-    if final[-len(w):] == w:
-        for j in w:
-            final.pop()
-print(''.join(final) if len(final) > 0 else 'FRULA')
+
+string = input()
+bomb = list(input())
+answer = []
+for i in string:
+    answer.append(i)
+    if answer[-len(bomb):] == bomb:
+        for j in range(len(bomb)):
+            answer.pop()
+print(''.join(answer) if answer else 'FRULA')
