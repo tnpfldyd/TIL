e34ac2c709942d6a4284dea1af00288c5448664b
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-01-16 15:17:42 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -0,0 +1,8 @@
+def solution(seoul):
+    idx = 0
+    for i in range(len(seoul)):
+        if seoul[i] == 'Kim':
+            idx = i
+            break
+    answer = '김서방은 ' + str(idx) + '에 있다'
+    return answer
