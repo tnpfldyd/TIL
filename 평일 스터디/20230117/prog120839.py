059762bb036d145d63753897982ec6c5ef9584e4
tnpfldyd <dkdldhs0322@gmail.com>
2023-01-17 16:40:00 +0900

    01/17 알고리즘

---
+++ 평일 스터디/20230117/prog120839.py
@@ -0,0 +1,9 @@
+
+def solution(rsp):
+    rsp_dic = {'2':'0', '0':'5', '5':'2'}
+    answer = ''
+    for rs in rsp:
+        answer += rsp_dic[rs]
+    return answer
+
+print(solution("205"))
---
+++ 평일 스터디/20230117/prog120866.py
@@ -0,0 +1,21 @@
+
+def solution(board):
+    dx, dy = [0,0,1,-1,1,1,-1,-1], [1,-1,0,0,1,-1,1,-1]
+    N = len(board)
+    temp = []
+    cnt = set()
+    for i in range(N):
+        for j in range(N):
+            if board[i][j]:
+                temp.append((i, j))
+                cnt.add((i, j))
+    for x, y in temp:
+        for i in range(8):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N:
+                board[nx][ny] = 1
+                cnt.add((nx, ny))
+    answer = N*N - len(cnt)
+    return answer
+
+print(solution([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]]))
