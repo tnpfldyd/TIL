39b7497485e8aeeef0d5e4b1e3ec7aeae1f5cf16
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-01-25 15:24:30 +0900

    [unrated] Title: 이모티콘 할인행사, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,24 +1,24 @@
 import itertools
 def solution(users, emoticons):
-    sale = [10, 20, 30, 40]
-    result = [0, 0]
-    for sales in itertools.product(sale, repeat=len(emoticons)):
-        emoticon = []
-        for i in range(len(emoticons)):
-            emoticon.append(emoticons[i] - (emoticons[i] * sales[i] // 100))
-        temp = [0, 0]
-        for i in range(len(users)):
-            users_temp = 0
-            for j in range(len(emoticons)):
-                if users[i][0] <= sales[j]:
-                    users_temp += emoticon[j]
-            if users_temp >= users[i][1]:
-                temp[0] += 1
+    temp = [10, 20, 30, 40]
+    person, result = 0, 0
+    for i in itertools.product(temp, repeat=len(emoticons)):
+        sign, total = 0, 0
+        for user in users:
+            dis, money = user
+            cnt = 0
+            for idx, j in enumerate(i):
+                if j >= dis:
+                    cnt += emoticons[idx] - (emoticons[idx] * j // 100)
+            if cnt >= money:
+                sign += 1
             else:
-                temp[1] += users_temp
-        if result[0] < temp[0]:
-            result = temp
-        elif result[0] == temp[0]:
-            if result[1] < temp[1]:
-                result = temp
-    return result
+                total += cnt
+        print(sign, total)
+        if person < sign:
+            person = sign
+            result = total
+        elif person == sign:
+            if result < total:
+                result = total
+    return [person, result]
