e7b80ca2ae3c10fd3a58bcc893593a2d35957f5e
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-02-02 11:08:29 +0900

    [Gold IV] Title: 알고스팟, Time: 200 ms, Memory: 116644 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,28 +1,24 @@
 from heapq import heappop, heappush
 import sys
-INF = sys.maxsize
 input = sys.stdin.readline
-M, N = map(int, input().split())
-
-matrix = [list(input().strip()) for _ in range(N)]
-
-visited = [[INF] * M for _ in range(N)]
-visited[0][0] = 0
-
+N, M = map(int, input().split())
+matrix = [list(map(int,input().rstrip())) for _ in range(M)]
+visited = [[0]*N for _ in range(M)]
 start = []
-heappush(start, (0, 0, 0))
-dx, dy = [0,0,1,-1],[1,-1,0,0]
+heappush(start, [0, 0, 0])
+visited[0][0] = 1
+dx, dy = [0,0,1,-1], [1,-1,0,0]
 while start:
-    cnt, x, y = heappop(start)
-    if (x, y) == (N-1, M-1):
-        print(cnt)
+    stone, x, y = heappop(start)
+    if x == M-1 and y == N-1:
+        print(stone)
         break
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx < N and 0 <= ny < M:
-            next_cost = cnt
-            if matrix[nx][ny] == '1':
-                next_cost = cnt + 1
-            if visited[nx][ny] > next_cost:
-                visited[nx][ny] = next_cost
-                heappush(start, (next_cost, nx, ny))
+        if 0 <= nx < M and 0 <= ny < N and not visited[nx][ny]:
+            if matrix[nx][ny] == 1:
+                visited[nx][ny] = 1
+                heappush(start, [stone+1, nx, ny])
+            else:
+                visited[nx][ny] = 1
+                heappush(start, [stone, nx, ny])
