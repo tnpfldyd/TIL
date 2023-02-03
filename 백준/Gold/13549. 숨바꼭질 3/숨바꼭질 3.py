758bd2cb9b22399b45f2c6fc2bdf1c557cc72bcf
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-02-03 16:21:14 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 192 ms, Memory: 37180 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,21 +1,22 @@
-from collections import deque
 import sys
-input = sys.stdin.readline
-N, K = map(int, input().rstrip().split())
-list_ = [-1] * 100001
-start = deque()
-start.append(N)
-list_[N] = 0
+from heapq import heappop, heappush
+INF = sys.maxsize
+S, E = map(int, input().split())
+visited = [INF] * 100001
+start = []
+heappush(start, (0, S))
+visited[S] = 0
 while start:
-    x = start.popleft()
-    if x == K:
-        print(list_[x])
+    cnt, x = heappop(start)
+    if x == E:
+        print(cnt)
         break
-    for i in (x*2, x-1, x+1):
-        if i == x*2:
-            if 0 <= i < 100001 and list_[i] == -1:
-                list_[i] = list_[x]
-                start.append(i)
-        elif 0 <= i < 100001 and list_[i] == -1:
-            list_[i] = list_[x] + 1
-            start.append(i)
+    for i in (x-1, x+1, x*2):
+        if 0 <= i < 100001:
+            if i == x*2:
+                next_cost = cnt
+            else:
+                next_cost = cnt + 1
+            if visited[i] > next_cost:
+                visited[i] = next_cost
+                heappush(start, (next_cost, i))
