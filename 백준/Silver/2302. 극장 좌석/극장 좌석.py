79332bc1d77ddcb4238c25f9746a493c5360522b
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-02-03 14:58:19 +0900

    [Silver I] Title: 극장 좌석, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.py
@@ -0,0 +1,24 @@
+N, M = int(input()), int(input())
+
+dp = [0] * 41
+dp[1], dp[2], dp[3], dp[4] = 1, 2, 3, 5
+for i in range(5, N+1):
+    dp[i] = dp[i-1] + dp[i-2]
+visited = [False] * (N + 1)
+for _ in range(M):
+    visited[int(input())] = True
+cnt = 0
+answer = []
+for i in range(1, N+1):
+    if not visited[i]:
+        cnt += 1
+    else:
+        if cnt:
+            answer.append(cnt)
+            cnt = 0
+if cnt:
+    answer.append(cnt)
+result = 1
+for i in answer:
+    result *= dp[i]
+print(result)
