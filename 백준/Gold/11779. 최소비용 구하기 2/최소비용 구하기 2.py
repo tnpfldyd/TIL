d4aea4f3c8af0eaa8e32fc33507698d694e811ed
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-02-07 17:06:52 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 576 ms, Memory: 142600 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -4,26 +4,26 @@ INF = sys.maxsize
 input = sys.stdin.readline
 N = int(input())
 M = int(input())
-visited = [[INF, []] for _ in range(N)]
+visited = [INF] * N
 matrix = [[] for _ in range(N)]
 for _ in range(M):
     a, b, t = map(int, input().split())
     matrix[a-1].append((t, b-1))
 s, e = map(int, input().split())
 start = []
-heappush(start, [0, s-1, [s]])
-visited[s-1][0] = 0
+heappush(start, (0, s-1, [s]))
+visited[s-1] = 0
 while start:
     x, node, load = heappop(start)
-    if x > visited[node][0]:
+    if x > visited[node]:
         continue
+    if node == e-1:
+        print(x)
+        print(len(load))
+        print(*load)
     for T, k in matrix[node]:
         nx = T + x
         nload = load + [k+1]
-        if visited[k][0] > nx:
-            visited[k][0] = nx
-            visited[k][1] = nload
-            heappush(start, [nx, k, nload])
-print(visited[e-1][0])
-print(len(visited[e-1][1]))
-print(*visited[e-1][1])
+        if visited[k] > nx:
+            visited[k] = nx
+            heappush(start, (nx, k, nload))
