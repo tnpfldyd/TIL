aa5568e568ad96dfdb245698477aabba2f41a66e
tnpfldyd <dkdldhs0322@gmail.com>
2023-02-15 08:28:48 +0900

    02/14 알고리즘

---
+++ 평일 스터디/20230214/boj5972.py
@@ -0,0 +1,25 @@
+from heapq import heappop, heappush
+import sys
+input = sys.stdin.readline
+INF = sys.maxsize
+N, M = map(int, input().split())
+matrix = [[] for _ in range(N)]
+visited = [INF] * N
+visited[0] = 0
+start = []
+heappush(start, (0, 0))
+for i in range(M):
+    a, b, t = map(int, input().split())
+    a -= 1; b -= 1
+    matrix[a].append((t, b))
+    matrix[b].append((t, a))
+while start:
+    x, node = heappop(start)
+    if x > visited[node]:
+        continue
+    for k, v in matrix[node]:
+        nx = x + k
+        if visited[v] > nx:
+            visited[v] = nx
+            heappush(start, (nx, v))
+print(visited[N-1])
