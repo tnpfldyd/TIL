c72ddc4e7bcd6d9b3ee6e485b36e3e27844bc472
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-02-20 09:16:44 +0900

    [Silver II] Title: 가장 큰 증가 부분 수열, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.py
@@ -1,11 +1,8 @@
 N = int(input())
 numbers = list(map(int, input().split()))
-dp = [0] * N
+dp = [0] * 1001
 answer = 0
-for i in range(N):
-    dp[i] = numbers[i]
-    for j in range(i):
-        if numbers[i] > numbers[j]:
-            dp[i] = max(dp[i], dp[j] + numbers[i])
-    answer = max(answer, dp[i])
+for number in numbers:
+    dp[number] = max(dp[:number]) + number
+    answer = max(answer, dp[number])
 print(answer)
