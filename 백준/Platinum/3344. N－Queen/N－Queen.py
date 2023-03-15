0109d3ceadd79e6e4b5251120f21cb4b98b8c612
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-03-15 11:06:52 +0900

    [Platinum III] Title: N-Queen, Time: 112 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Platinum/3344. N－Queen/N－Queen.py
@@ -0,0 +1,25 @@
+N = int(input())
+isOdd = False
+
+if N % 2:
+    isOdd = True
+
+if (not isOdd and N % 6 != 2) or (isOdd and (N - 1) % 6 != 2):
+    if isOdd:
+        N -= 1
+    for i in range(1, N // 2 + 1):
+        print(2 * i)
+    for i in range(1, N // 2 + 1):
+        print(2 * i - 1)
+    if isOdd:
+        print(N + 1)
+
+elif (not isOdd and N // 6) or (isOdd and (N - 1) // 6 != 2):
+    if isOdd:
+        N -= 1
+    for i in range(1, N // 2 + 1):
+        print(1 + (2 * i + N // 2 - 3) % N)
+    for i in range(N // 2, 0, -1):
+        print(N - (2 * i + N // 2 - 3) % N)
+    if isOdd:
+        print(N + 1)
