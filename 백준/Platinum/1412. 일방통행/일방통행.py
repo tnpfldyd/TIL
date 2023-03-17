3fe74eb5ae5fcc6ca5d2e9d995c6e4da5698b051
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-03-17 17:34:00 +0900

    [Platinum IV] Title: 일방통행, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Platinum/1412. 일방통행/일방통행.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+matrix = [list(input().strip()) for _ in range(N)]
+
+result = [[False] * N for _ in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        if matrix[i][j] == 'Y' and matrix[j][i] == 'N':
+            result[i][j] = True
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if result[i][k] and result[k][j]:
+                result[i][j] = True
+
+for i in range(N):
+    if result[i][i]:
+        print('NO')
+        break
+else:
+    print('YES')
