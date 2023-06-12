3478c4770916724324b700c5e8071d35c11e5c13
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-06-12 16:25:19 +0900

    [Gold IV] Title: N-Queen, Time: 31868 ms, Memory: 204952 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,23 @@
+N = int(input())
+
+arr = [0] * N
+result = 0
+
+def check(x):
+    for i in range(x):
+        if arr[i] == arr[x] or abs(arr[x] - arr[i]) == x - i:
+            return False
+    return True
+
+def dfs(x):
+    if x == N:
+        global result
+        result += 1
+        return
+    for i in range(N):
+        arr[x] = i
+        if check(x):
+            dfs(x + 1)
+
+dfs(0)
+print(result)
