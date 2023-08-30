fcea2b01f811320fdb7d0c6f86a926d40baf8339
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-08-30 12:17:04 +0900

    [Gold IV] Title: 합이 0, Time: 412 ms, Memory: 115584 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.py
+++ 백준/Gold/3151. 합이 0/합이 0.py
@@ -1,23 +1,10 @@
-from bisect import bisect_left
-
 N = int(input())
-numbers = sorted(list(map(int, input().split())))
-
+numbers = list(map(int, input().split()))
+dp = [0] * 30001
 answer = 0
-for i in range(N - 2):
-    l, r = i + 1, N - 1
-    while l < r:
-        cur, left, right = numbers[i], numbers[l], numbers[r]
-        result = cur + left + right
-        if result > 0:
-            r -= 1
-        else:
-            if not result:
-                if left == right:
-                    answer += r - l
-                else:
-                    idx = bisect_left(numbers, right)
-                    answer += r - idx + 1
-            l += 1
+for i in range(1, N):
+    answer += dp[10000 - numbers[i]]
+    for j in range(i):
+        dp[10000 + numbers[i] + numbers[j]] += 1
 
 print(answer)
