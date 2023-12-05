61547100e7e622aae5e4d642d2aacf38df7de00f
Yong <108651414+tnpfldyd@users.noreply.github.com>
2023-12-05 09:25:15 +0900

    [Gold V] Title: 카드게임, Time: 108 ms, Memory: 108080 KB, Score: 64 point -BaekjoonHub

---
+++ 백준/Gold/10835. 카드게임/카드게임.py
@@ -0,0 +1,20 @@
+N = int(input())
+left_cards = list(map(int, input().split()))
+right_cards = list(map(int, input().split()))
+
+dp = [[-1] * N for _ in range(N)]
+
+def get_result(left, right):
+    if left == N or right == N:
+        return 0
+    if dp[left][right] != -1:
+        return dp[left][right]
+    dp[left][right] = 0
+    if left_cards[left] > right_cards[right]:
+        dp[left][right] += right_cards[right] + get_result(left, right + 1)
+    else:
+        dp[left][right] += max(get_result(left + 1, right), get_result(left + 1, right + 1))
+    
+    return dp[left][right]
+
+print(get_result(0, 0))
