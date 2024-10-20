c32b911c7d7dc0b5fdb693379df1705f5f965918
Yong <108651414+tnpfldyd@users.noreply.github.com>
2024-10-20 10:29:17 +0900

    [Gold IV] Title: KSA 문자열, Time: 120 ms, Memory: 111748 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31433. KSA 문자열/KSA 문자열.py
+++ 백준/Gold/31433. KSA 문자열/KSA 문자열.py
@@ -1,20 +1,33 @@
-KSA = "KSA"
-def cal(target_string):
-    idx = 0
+def main():
+    print(solution())
+
+def solution():
+    input_str = input()
+    return solution_helper(input_str)
+
+def solution_helper(input_str):
+    length = len(input_str)
+    count1 = count_pattern(input_str, "KSA")
+    count2 = count_pattern(input_str, "SAK")
+    if count2 == length:
+        count2 -= 1
+    count3 = count_pattern(input_str, "AKS")
+    if count3 >= length - 1:
+        count3 = length - 2
+    max_count = max(count1, count2, count3)
+    answer = 2 * (length - max_count)
+    return answer
+
+def count_pattern(s, pattern):
+    index = 0
     count = 0
-    for char in target_string:
-        if char == KSA[idx % 3]:
-            idx += 1
-        else:
+    for char in s:
+        if char == pattern[index]:
+            index += 1
             count += 1
-    count += abs(idx - len(X))
+            if index == len(pattern):
+                index = 0
     return count
 
-X = input().strip()
-answer = float('inf')
-
-answer = min(answer, cal(X))
-answer = min(answer, cal("K" + X))
-answer = min(answer, cal("KS" + X))
-
-print(answer)
+if __name__ == "__main__":
+    main()
