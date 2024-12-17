d58072a694b79d9f1bb370e0949b1fbc1c911a77
Yong <108651414+tnpfldyd@users.noreply.github.com>
2024-12-17 09:43:14 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 140 ms, Memory: 187076 KB -BaekjoonHub

---
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.py
@@ -0,0 +1,14 @@
+n = int(input())
+s = [0, 1]
+
+for i in range(2, abs(n) + 1):
+    s.append((s[i - 1] + s[i - 2]) % 1000000000)
+
+if n % 2 == 0 and n < 0:
+    print(-1)
+elif n == 0:
+    print(0)
+else:
+    print(1)
+
+print(s[abs(n)])
