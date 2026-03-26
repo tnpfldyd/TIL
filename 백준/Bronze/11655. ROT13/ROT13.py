f2ea7cfcdea9c87968dd0f9ab2fb806e56641d55
Yong <108651414+tnpfldyd@users.noreply.github.com>
2026-03-27 07:33:58 +0900

    [Bronze I] Title: ROT13, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,8 @@
+s = input()
+
+table = str.maketrans(
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+    'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+)
+
+print(s.translate(table))
