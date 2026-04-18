39a6a853947d56a8efa6975c588f0bffa9bbbbee
Yong <108651414+tnpfldyd@users.noreply.github.com>
2026-04-18 17:16:50 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+x = input().strip()
+
+if x == 'M':
+    print("MatKor")
+elif x == 'W':
+    print("WiCys")
+elif x == 'C':
+    print("CyKor")
+elif x == 'A':
+    print("AlKor")
+elif x == '$':
+    print("$clear")
